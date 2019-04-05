fda6a7d07d46067667744a8d9633f7c29bcc52cb
Anwar Abdullah <anwarabdullahn28@gmail.com>
2019-04-06 01:05:43 +0700

    Updates

---
+++ precache-manifest.87dcdd68da9548bcff8de7e6e1a765e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f35f0702b824f4ffd953",
+    "url": "/try-moovaz-react/static/js/runtime~main.b3d7ebd3.js"
+  },
+  {
+    "revision": "75fee801cc15ca45e089",
+    "url": "/try-moovaz-react/static/js/main.0fa30aaf.chunk.js"
+  },
+  {
+    "revision": "11a0bfc947ab161c0f63",
+    "url": "/try-moovaz-react/static/js/2.0e57b4b0.chunk.js"
+  },
+  {
+    "revision": "11a0bfc947ab161c0f63",
+    "url": "/try-moovaz-react/static/css/2.f12cf2f7.chunk.css"
+  },
+  {
+    "revision": "ec2c2416888b1cc487e4304dd4af962e",
+    "url": "/try-moovaz-react/index.html"
+  }
+];
